a5daa5b877873bb14dddb972a1bb277e6bdcff51
Kidus Silieshi <kiduss@esl.et>
2024-03-21 17:27:39 +0300

    New update

--- php/teacherDoc.php
+++ php/teacherDoc.php
@@ -1,10 +1,47 @@
 <?php
-    session_start();
 
-    $IDCard = $_POST['IDc'];
-    $EduDoc = $_POST['EDd'];
-    $TCV = $_POST['CRv'];
+    include 'connection.php';
 
-    echo $IDCard."<br>".$EduDoc."<br>".$TCV;
-    
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["IDc"])) {
+
+        $targetDir = "../teacherfiles/";
+
+        $fileName = $_FILES["IDc"]["name"];
+        $targetFile = $targetDir . $fileName;
+
+        if(move_uploaded_file($_FILES["IDc"]["tmp_name"], $targetFile)) {
+            echo "The file ".$fileName." has been upload successfully.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["EDd"])) {
+
+        $targetDir = "../teacherfiles/";
+
+        $fileName = $_FILES["EDd"]["name"];
+        $targetFile = $targetDir . $fileName;
+
+        if(move_uploaded_file($_FILES["EDd"]["tmp_name"], $targetFile)) {
+            echo "The file ".$fileName." has been upload successfully.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["CRv"])) {
+
+        $targetDir = "../teacherfiles/";
+
+        $fileName = $_FILES["CRv"]["name"];
+        $targetFile = $targetDir . $fileName;
+
+        if(move_uploaded_file($_FILES["CRv"]["tmp_name"], $targetFile)) {
+            echo "The file ".$fileName." has been upload successfully.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
 ?>
