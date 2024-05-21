0334b01f00af783f5d6731e41bb31a7534e4dbef
kiyuenter <kiyuenterprise@gmail.com>
2024-05-20 20:23:46 -0700

    Update content

--- php/generate_report.php
+++ php/generate_report.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
-    include "connection.php"
-    
+    include "connection.php";
+
+    $reportType = $_POST['product_id'];
+    $date_from = $_POST['date_from'];
+    $date_to = $_POST['date_to'];
 ?>
